1c2625a1ba0b29e789e015b8aa492d969c59f265
Nicanor50 <akpovobarachie@email.com>
2025-08-26 16:16:26 +0100

    Troisième commit avec les pages du site faites pour la publication

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./Components/AuthContext";
 import { PanierContextProvider } from "./Store/Panier_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <PanierContextProvider>
           <App />
         </PanierContextProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
