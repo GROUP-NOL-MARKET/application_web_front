97033098b2166b67e70ab3c9ba6d5a713d0f22ae
Nicanor50 <akpovobarachie@email.com>
2025-09-01 12:45:54 +0100

    Updates

--- src/Components/extractError.js
+++
@@ -1,9 +0,0 @@
- const extractError = (object) => {
-        const messages = [];
-        for (const [key, value] of Object.entries(object)) {
-            value.forEach(msg => messages.push(msg));
-        }
-        return messages;
-}
-
-export default extractError;
