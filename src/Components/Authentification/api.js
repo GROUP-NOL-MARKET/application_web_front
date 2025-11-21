8e2c5a20bf5a2ee874ae9cdfd6d5676759fd2259
GROUP-NOL-MARKET <groupnolmarket@gmail.com>
2025-11-21 15:11:56 +0100

    Commit après le changement du chargement des images http

--- src/Components/Authentification/api.js
+++ src/Components/Authentification/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // ======================================
 //   BASE URL depuis .env
 // ======================================
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://nol-market-back.onrender.com/api";
 
 // process.env.REACT_APP_API_URL ||
 
